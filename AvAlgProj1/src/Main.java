b622d1f2b1d1ca039a9b6762b5e0875a2767594f
Fredric <fericss@gmail.com>
2013-10-05 04:18:14 +0200

    durpdedup

--- AvAlgProj1/src/Main.java
+++ AvAlgProj1/src/Main.java
@@ -1,20 +1,51 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 
 
 public class Main {
 
 	
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-		BigInteger b = BigInteger.valueOf(31);
-		System.out.println(b.isProbablePrime(99));
+	public static void main(String[] args2){
+//		System.out.println("Hello world!");
+//		BigInteger b = BigInteger.valueOf(31);
+//		System.out.println(b.isProbablePrime(99));
 		
-		int testNumber = 65536*2*3*5;
+		/// 262144 tested, and does not run out of memory on kattis: Accepted (0) 	0.88 s 	Java
+		/// (not saying 262144 is a good limit or anything, just that it's the maximum amount of memory we're allowed)
+		
+		int testNumber = 262144;
 		FactoringPrimeSieve fac = new FactoringPrimeSieve(testNumber);
-		System.out.print(testNumber+": ");
-		for(int i : fac.numbers[testNumber].getFactors()){
-			System.out.print(i+ " ");
+		
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			double a = sc.nextDouble();
+			if(a>testNumber){
+				System.out.println("fail\n");
+				continue;
+			}
+			int num;
+			try{
+				num = (int) a;
+				if(num>testNumber){
+					System.out.println("fail\n");
+					continue;
+				}
+			}
+			catch(Exception e){
+				System.out.println("fail\n");
+				continue;
+			}
+			for(int i :  fac.numbers[num].getFactors()){
+				System.out.println(i);
+			}
+			System.out.println();
 		}
+		
+		
+//		System.out.print(testNumber+": ");
+//		for(int i : fac.numbers[testNumber].getFactors()){
+//			System.out.print(i+ " ");
+//		}
 		
 	}
 	
